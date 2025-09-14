601affb6da3f91cccb47957e3753cebc9e873e45
HellGa <esfero1307@gmail.com>
2025-09-14 21:06:00 +0300

    Fix client-side routing

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,13 @@ export default defineConfig({
             return 'assets/[name]-[hash][extname]';
           }
           return 'assets/[name]-[hash][extname]';
-        }
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
       }
-    }
+    },
+    target: 'es2015',
+    minify: 'terser'
   },
   base: './',
   publicDir: 'public',
